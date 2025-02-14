b44e857b6706b8d26a996813f55f87d4d3a9bde6
Svettsovvv <andreysvetcov@gmail.com>
2025-02-14 22:38:12 +0300

    Исправлено: переименованы файлы lesson4_3.py и lesson4_4.py

--- lesson_4/lesson4_3.py
+++ lesson_4/lesson4_3.py
@@ -1,12 +1,44 @@
 import pandas as pd
 
-ser1 = pd.Series(['a', 'b', 'c'], index=[1, 2, 3])
-ser2 = pd.Series(['d', 'e', 'f'], index=[4, 5, 6])
+index = [
+    ('city_1', 2010),
+    ('city_1', 2020),
+    ('city_2', 2010),
+    ('city_2', 2020),
+    ('city_3', 2010),
+    ('city_3', 2020),
+]
 
-print(ser1)
+population = [
+    101,
+    201,
+    102,
+    202,
+    103,
+    203,
+]
 
-print(ser2)
+pop = pd.Series(population, index=index)
 
-print(pd.concat([ser1, ser2], axis=1, join='outer'))
+pop_df = pd.DataFrame(
+    {
+        'total': pop,
+        'something': [
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+        ]
+    }
+)
 
-print(pd.concat([ser1, ser2], axis=1, join='inner'))
+print(pop_df)
+
+pop_df_1 = pop_df.loc[('city_1', slice(None)), 'something']
+print(pop_df_1)
+
+pop_df_1 = pop_df.loc[[('city_1', 'city_3')], ['total', 'something']]
+print(pop_df_1)
+
--- lesson_4/lesson4_4.py
+++ lesson_4/lesson4_4.py
@@ -1,44 +1,12 @@
 import pandas as pd
 
-index = [
-    ('city_1', 2010),
-    ('city_1', 2020),
-    ('city_2', 2010),
-    ('city_2', 2020),
-    ('city_3', 2010),
-    ('city_3', 2020),
-]
+ser1 = pd.Series(['a', 'b', 'c'], index=[1, 2, 3])
+ser2 = pd.Series(['d', 'e', 'f'], index=[4, 5, 6])
 
-population = [
-    101,
-    201,
-    102,
-    202,
-    103,
-    203,
-]
+print(ser1)
 
-pop = pd.Series(population, index=index)
+print(ser2)
 
-pop_df = pd.DataFrame(
-    {
-        'total': pop,
-        'something': [
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-        ]
-    }
-)
+print(pd.concat([ser1, ser2], axis=1, join='outer'))
 
-print(pop_df)
-
-pop_df_1 = pop_df.loc[('city_1', slice(None)), 'something']
-print(pop_df_1)
-
-pop_df_1 = pop_df.loc[[('city_1', 'city_3')], ['total', 'something']]
-print(pop_df_1)
-
+print(pd.concat([ser1, ser2], axis=1, join='inner'))
